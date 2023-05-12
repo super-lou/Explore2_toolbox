502270853f9c982a308f40b847dc3ef8e8da82ef
louis.heraut <louis.heraut@inrae.fr>
2023-05-12 11:01:24 +0200

    debug

--- main.R
+++ main.R
@@ -252,7 +252,7 @@ MPI =
 period_analyse_diag = c('1976-01-01', '2019-12-31')
 period_analyse_proj = c('1975-09-01', '2100-08-31')
 propagate_NA = TRUE
-nCode4RAM = 10
+nCode4RAM = 15
 use_proj_merge =
     # TRUE
     FALSE
