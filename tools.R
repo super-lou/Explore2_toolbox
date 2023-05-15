dd1760d7184e93530b138a024b4a2a1fc8111435
louis.heraut <louis.heraut@inrae.fr>
2023-05-15 17:32:03 +0200

    meso ready

--- tools.R
+++ tools.R
@@ -220,13 +220,17 @@ NetCDF_to_tibble = function (NetCDF_path,
         Q_sim = Q_sim[CodeOrder,,drop=FALSE]
         Q_sim = c(t(Q_sim))
 
-        # if ("topologicalSurface_model" %in% names(NCdata$var)) {
-        S = ncdf4::ncvar_get(NCdata, "topologicalSurface_model",
-                             start=start,
-                             count=count)
+        if ("topologicalSurface_model" %in% names(NCdata$var)) {
+            S = ncdf4::ncvar_get(NCdata, "topologicalSurface_model",
+                                 start=start,
+                                 count=count)
+        } else {
+            S = ncdf4::ncvar_get(NCdata, "surface_model",
+                                 start=start,
+                                 count=count)
+        }
         S = S[station]
         S = S[CodeOrder]
-        # }
         # else {
             # S = rep(NA, times=nCode)
         # }
