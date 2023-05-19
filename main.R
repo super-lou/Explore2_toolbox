573af611ec0d1694fa1becc1f8128e5e9cae8018
louis.heraut <louis.heraut@inrae.fr>
2023-05-19 16:56:19 +0200

    dzaiuhdaihdzaiuhfeza

--- main.R
+++ main.R
@@ -275,13 +275,13 @@ models_to_use =
     c(
         # "CTRIP"
         # "EROS"
-        # "GRSD"
+        "GRSD"
         # "J2000"
         # "SIM2"
         # "MORDOR-SD"
         # "MORDOR-TS"
         # "ORCHIDEE"
-        "SMASH"
+        # "SMASH"
     )
 complete_by = "SMASH"
 
