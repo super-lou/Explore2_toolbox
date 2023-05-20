0ea0fbf1f88567db1ab2dd3618e4184c88032393
louis.heraut <louis.heraut@inrae.fr>
2023-05-20 15:04:52 +0200

    dzaiuhdaihdzaiuhfeza

--- main.R
+++ main.R
@@ -166,10 +166,10 @@ to_do =
 analyse_data =
     c(
         # "WIP"
-        'Explore2_diag_criteria_all',
-        'Explore2_diag_criteria_select',
-        'Explore2_diag_serie'
-        # 'Explore2_diag_proj_serie'
+        # 'Explore2_diag_criteria_all',
+        # 'Explore2_diag_criteria_select',
+        # 'Explore2_diag_serie'
+        'Explore2_diag_proj_serie'
         # 'Explore2_proj_serie',
         # 'Explore2_proj_check'
         # 'Explore2_proj_delta'
