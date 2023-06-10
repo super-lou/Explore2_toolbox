128ce78b08406b21e9bda2803d2f2b80ed454d17
louis.heraut <louis.heraut@inrae.fr>
2023-06-10 03:06:12 +0200

    meso ready

--- main.R
+++ main.R
@@ -166,13 +166,13 @@ to_do =
 analyse_data =
     c(
         # 'WIP'
-        'Explore2_criteria_diag_performance',
-        'Explore2_criteria_diag_sensibilite',
-        'Explore2_criteria_diag_sensibilite_RAT'
-        # 'Explore2_criteria_diag_HE',
-        # 'Explore2_criteria_diag_ME',
-        # 'Explore2_criteria_diag_BE',
-        # 'Explore2_criteria_diag_BF'
+        # 'Explore2_criteria_diag_performance',
+        # 'Explore2_criteria_diag_sensibilite',
+        # 'Explore2_criteria_diag_sensibilite_RAT'
+        'Explore2_criteria_diag_HE',
+        'Explore2_criteria_diag_ME',
+        'Explore2_criteria_diag_BE',
+        'Explore2_criteria_diag_BF'
         # 'Explore2_serie_diag_plot'
         # 'Explore2_serie_proj_safran',
         # 'Explore2_more_serie_proj_safran'
