b6a9657b11c8a98f71ddbf0bb3f3b5aa941ac283
louis.heraut <louis.heraut@inrae.fr>
2023-07-28 14:10:05 +0200

    new job

--- main.R
+++ main.R
@@ -158,7 +158,7 @@ to_do =
         # 'merge_nc'
         # 'reshape_data'
         'create_data',
-        'extract_data',
+        # 'extract_data',
         'save_extract'
         # 'read_tmp'
         # 'read_saving',
@@ -299,7 +299,7 @@ propagate_NA = TRUE
 # nCode4RAM | 20 | 20
 # nodes     |  3 |  2
 # tasks     | 28 | 28
-nCode4RAM = 9
+nCode4RAM = 100
 
 projs_to_use =
     c(
