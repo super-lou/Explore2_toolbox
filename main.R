2aa1e45015bd04a869707013bef9a9a76c0b04d5
louis.heraut <louis.heraut@inrae.fr>
2023-08-07 01:13:04 +0200

    new job

--- main.R
+++ main.R
@@ -181,9 +181,9 @@ extract_data =
         # 'Explore2_criteria_diag_ME'
         # 'Explore2_criteria_diag_BE',
         # 'Explore2_criteria_diag_BF'
-        'Explore2_serie_diag_plot'
-        # 'Explore2_serie_proj_safran',
-        # 'Explore2_serie_more_proj_safran'
+        # 'Explore2_serie_diag_plot'
+        'Explore2_serie_proj_safran',
+        'Explore2_serie_more_proj_safran'
         # 'Explore2_serie_proj'
         # 'Explore2_serie_more_proj'
     )
