5c1c04f44f954129f12360be70aa39974ab6fbe8
louis.heraut <louis.heraut@inrae.fr>
2023-10-27 02:32:59 +0200

    fdzefez

--- main.R
+++ main.R
@@ -159,7 +159,7 @@ to_do =
         # 'merge_nc'
         # 'reshape_data',
         'create_data',
-        'extract_data',
+        # 'extract_data',
         'save_extract'
         # 'read_tmp'
         # 'read_saving',
@@ -302,7 +302,7 @@ propagate_NA = TRUE
 # nCode4RAM | 20 | 20
 # nodes     |  3 |  2
 # tasks     | 28 | 28
-nCode4RAM = 14
+nCode4RAM = 100
 
 projs_to_use =
     c(
