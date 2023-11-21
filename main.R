23b4b721daca862ed548a15502c20077816dab92
louis.heraut <louis.heraut@inrae.fr>
2023-11-21 23:04:27 +0100

    new job

--- main.R
+++ main.R
@@ -338,12 +338,12 @@ models_to_use =
     c(
         # "CTRIP"
         # "EROS",
-        "GRSD",
-        "J2000"
+        # "GRSD",
+        # "J2000"
         # "SIM2",
         # "MORDOR-SD",
         # "MORDOR-TS",
-        # "ORCHIDEE",
+        "ORCHIDEE"
         # "SMASH"
 
         # "AquiFR",
