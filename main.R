c347970c7c6a0e7a56a594d83db5363ac48c62ec
louis.heraut <louis.heraut@inrae.fr>
2023-11-21 23:00:47 +0100

    new job

--- main.R
+++ main.R
@@ -156,8 +156,8 @@ mode =
 to_do =
     c(
         # 'delete_tmp',
-        'clean_nc'
-        # 'merge_nc'
+        # 'clean_nc'
+        'merge_nc'
         # 'reshape_data',
         # 'create_data',
         # 'extract_data',
@@ -295,8 +295,8 @@ period_extract_diag = c('1976-01-01', '2019-12-31')
 period_extract_projection = c('1975-09-01', '2100-08-31')
 period_reference = c("1976-01-01", "2005-12-31")
 is_projection_clean =
-    # TRUE
-    FALSE
+    TRUE
+    # FALSE
 is_projection_merge =
     # TRUE
     FALSE
@@ -338,12 +338,12 @@ models_to_use =
     c(
         # "CTRIP"
         # "EROS",
-        # "GRSD",
-        # "J2000",
+        "GRSD",
+        "J2000"
         # "SIM2",
         # "MORDOR-SD",
         # "MORDOR-TS",
-        "ORCHIDEE"
+        # "ORCHIDEE",
         # "SMASH"
 
         # "AquiFR",
