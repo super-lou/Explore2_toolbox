f31bfd047a90109d098f8b46bfdb1d403fc4f65b
louis.heraut <louis.heraut@inrae.fr>
2024-01-02 03:07:12 +0100

    new job

--- main.R
+++ main.R
@@ -340,13 +340,13 @@ projs_to_use =
 models_to_use =
     c(
         # "CTRIP"
-        "EROS"
+        # "EROS"
         # "GRSD"
         # "J2000"
         # "SIM2"
         # "MORDOR-SD"
         # "MORDOR-TS"
-        # "ORCHIDEE"
+        "ORCHIDEE"
         # "SMASH"
 
         # "AquiFR",
@@ -1049,9 +1049,31 @@ if (MPI != "") {
     library(Rmpi)
     rank = mpi.comm.rank(comm=0)
     size = mpi.comm.size(comm=0)
+
+    if (size > 1) {
+        if (rank == 0) {
+            Rrank_sample = sample(0:(size-1))
+            for (root in 1:(size-1)) {
+                Rmpi::mpi.send(as.integer(Rrank_sample[root+1]),
+                               type=1, dest=root,
+                               tag=1, comm=0)
+            }
+            Rrank = Rrank_sample[1]
+        } else {
+            Rrank = Rmpi::mpi.recv(as.integer(0),
+                                   type=1,
+                                   source=0,
+                                   tag=1, comm=0)
+        }
+    } else {
+        Rrank = 0
+    }
+    post(paste0("Random rank attributed : ", Rrank))
+    
 } else {
     rank = 0
     size = 1
+    Rrank = 0
 }
 
 
@@ -1346,10 +1368,9 @@ if (type == "hydrologie") {
         names(Subsets)[length(Subsets)] = paste0(name, n)
     }
     nSubsets = length(Subsets)
-    
 
     if (by_files | MPI == "file") {
-        if (MPI == "file") {
+        if (MPI == "file") {            
             start = ceiling(seq(1, nFiles_to_use,
                                 by=(nFiles_to_use/size)))
             if (any(diff(start) == 0)) {
@@ -1358,15 +1379,17 @@ if (type == "hydrologie") {
             } else {
                 end = c(start[-1]-1, nFiles_to_use)
             }
+
             if (rank == 0) {
                 post(paste0(paste0("rank ", 0:(size-1), " get ",
                                    end-start+1, " files"),
                             collapse="    "))
             }
-            if (rank+1 > nFiles_to_use) {
+            
+            if (Rrank+1 > nFiles_to_use) {
                 Files = NULL
             } else {
-                Files = files_to_use[start[rank+1]:end[rank+1]]
+                Files = files_to_use[start[Rrank+1]:end[Rrank+1]]
             }
             
         } else {
