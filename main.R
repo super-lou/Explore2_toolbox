80a4a4be72c35a61673d2264365811bbdfe73c87
louis.heraut <louis.heraut@inrae.fr>
2024-02-17 22:10:51 +0100

    i want to die

--- main.R
+++ main.R
@@ -359,8 +359,8 @@ HM_to_use =
         # "J2000"
         # "MORDOR-SD"
         # "MORDOR-TS"
-        "ORCHIDEE"
-        # "SIM2"
+        # "ORCHIDEE"
+        "SIM2"
         # "SMASH" 
 
         # "AquiFR",
