aa73957e69aa248032b788a31ae10b440d0e2c94
louis.heraut <louis.heraut@inrae.fr>
2024-02-19 16:58:04 +0100

    debug

--- main.R
+++ main.R
@@ -376,8 +376,8 @@ HM_to_use =
         # "GRSD"
         # "J2000"
         # "MORDOR-SD"
-        "MORDOR-TS"
-        # "ORCHIDEE"
+        # "MORDOR-TS"
+        "ORCHIDEE"
         # "SIM2"
         # "SMASH" 
 
