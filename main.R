405274ddf68de5c6c8f265b883b37ff38aa864cf
louis.heraut <louis.heraut@inrae.fr>
2024-02-27 14:02:15 +0100

    new job

--- main.R
+++ main.R
@@ -388,8 +388,8 @@ HM_to_use =
         # "J2000"
         # "MORDOR-SD"
         # "MORDOR-TS"
-        "ORCHIDEE"
-        # "SIM2"
+        # "ORCHIDEE"
+        "SIM2"
         # "SMASH" 
 
         # "AquiFR",
