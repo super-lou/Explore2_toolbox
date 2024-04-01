612b7303caded4911915a892af43aa9898271bff
louis.heraut <louis.heraut@inrae.fr>
2024-04-01 13:59:18 +0200

    new job

--- main.R
+++ main.R
@@ -449,8 +449,8 @@ HM_to_use =
         # "MORDOR-SD"
         # "MORDOR-TS"
         # "ORCHIDEE"
-        "SIM2"
-        # "SMASH" 
+        # "SIM2"
+        "SMASH" 
 
         # "AquiFR",
         # "EROS Bretagne",
