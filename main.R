25261e5a6230e9e45d0bf35121e572d45873a462
louis.heraut <louis.heraut@inrae.fr>
2024-04-02 10:53:03 +0200

    new job

--- main.R
+++ main.R
@@ -442,8 +442,8 @@ storylines =
 HM_to_use = 
     c(
         # "CTRIP"
-        "EROS"
-        # "GRSD"
+        # "EROS"
+        "GRSD"
         # "J2000"
         # "MORDOR-SD"
         # "MORDOR-TS"
