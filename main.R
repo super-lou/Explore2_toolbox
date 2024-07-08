60773d88df60200e60baf0c7c3c8a0b06cf2acd7
louis.heraut <louis.heraut@inrae.fr>
2024-07-08 17:27:23 +0200

    new jobv

--- main.R
+++ main.R
@@ -44,9 +44,9 @@
 ## 1. REQUIREMENTS ___________________________________________________
 # Explore2_toolbox path
 lib_path =
-    # "./" #botan / sacados
+    "./" #botan / sacados
     # '/home/lheraut/library/Explore2_toolbox' #ESPRI
-    '/home/herautl/library/Explore2_toolbox' #MUSE
+    # '/home/herautl/library/Explore2_toolbox' #MUSE
 
 ## 2. GENERAL PROCESSES ______________________________________________
 # This to_do vector regroups all the different step you want to do.
@@ -2110,5 +2110,5 @@ if ('create_database' %in% to_do) {
 
 if (MPI != "") {
     Sys.sleep(10)
-    mpi.finalize()
+    # mpi.finalize()
 }
